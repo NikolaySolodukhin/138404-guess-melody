ba9a4c748271cf56f57301373f712c0f7925f7f0
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-10-03 01:35:04 +0300

    Разбил на модули. Настроил логику переключения. Настроил сборку

--- js/main.js
+++ js/main.js
@@ -1,38 +1,10 @@
+import {showScreen} from './utils.js';
+import {screenWelcome, initScreenWelcome} from './screens/welcome.js';
 
-const template = document.getElementById(`templates`);
-const gameContainer = document.querySelector(`.main`);
-const arraySreensGame = Array.from((template.content.querySelectorAll(`.main`)));
-let currentIndex = 0;
+showScreen(screenWelcome);
+initScreenWelcome();
+
 const InuputKey = {
   RIGHT: 39,
   LEFT: 37
-};
-
-const showScreenGame = (screensAll, numberScreen) => {
-  while (gameContainer.hasChildNodes()) {
-    gameContainer.removeChild(gameContainer.lastChild);
-  }
-  gameContainer.appendChild(screensAll[numberScreen]);
-};
-
-showScreenGame(arraySreensGame, currentIndex);
-
-document.addEventListener(`keydown`, (evt) => {
-  if (evt.altKey && evt.keyCode === InuputKey.RIGHT) {
-    showScreenGame(arraySreensGame, getRightIndex(++currentIndex));
-  } else if (evt.altKey && evt.keyCode === InuputKey.LEFT) {
-    showScreenGame(arraySreensGame, getRightIndex(--currentIndex));
-  }
-  evt.stopPropagation();
-});
-
-const getRightIndex = (index) => {
-  if (index > arraySreensGame.length - 1) {
-    currentIndex = 0;
-    return currentIndex;
-  } else if (index < 0) {
-    currentIndex = arraySreensGame.length - 1;
-    return currentIndex;
-  }
-  return index;
 };
