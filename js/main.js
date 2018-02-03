46d682fcef6946479593b20d668df740ac9c7405
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2018-02-04 02:04:57 +0300

    remove gulp config. Add webpack config

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import 'babel-polyfill';
-import 'whatwg-fetch';
-
-import {GameSettings} from './data/game-play.js';
-import State from './data/state.js';
-import Application from './screens/application.js';
-
-Application.init(new State(GameSettings.MAX_GAME_TIME));
---
+++ src/js/polyfill/polyfills.js
@@ -0,0 +1,8 @@
+import "babel-core/register";
+import "babel-polyfill";
+import "whatwg-fetch";
+import "core-js/fn/promise";
+import "core-js/fn/array/from";
+import "core-js/fn/array/find";
+import "core-js/fn/object/assign";
+import "core-js/fn/object/values";
